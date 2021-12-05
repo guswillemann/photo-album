31f6288e2b82cc8e4c2a2a9ebf0ae0094217861b
Gustavo Willemann <guswillemann@gmail.com>
2021-12-05 20:09:08 -0300

    feat(PhotoCard): changes when to open details modal

--- src/components/PhotosList/PhotoCard/index.tsx
+++ src/components/PhotosList/PhotoCard/index.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import { PhotoData } from '..';
-import { IconButton, Modal } from '../..';
+import { Modal } from '../..';
 import { useToggle } from '../../../hooks';
 import PhotoDetails from './PhotoDetails';
 import styles from './styles.module.scss';
@@ -13,31 +13,31 @@ export default function PhotoCard({ photoData }: PhotoProps) {
   const [isModalOpen, toggleModal] = useToggle(false);
 
   return (
-    <li className={styles.wrapper}>
+    <>
+      <li
+        onClick={toggleModal}
+        className={styles.wrapper}
+        role="button"
+      >
+        <Image
+          src={photoData.src.large}
+          width={photoData.width}
+          height={photoData.height}
+          alt={`Photo by ${photoData.photographer}`}
+          placeholder="blur"
+          blurDataURL={photoData.src.small}
+        />
+        <div className={styles.info}>
+          <span>Photographer:</span>
+          <p>{photoData.photographer}</p>
+        </div>
+      </li>
       <Modal
         isOpen={isModalOpen}
         onClose={toggleModal}
       >
         <PhotoDetails photoData={photoData} /> 
       </Modal>
-      <Image
-        src={photoData.src.large}
-        width={photoData.width}
-        height={photoData.height}
-        alt={`Photo by ${photoData.photographer}`}
-        placeholder="blur"
-        blurDataURL={photoData.src.small}
-      />
-      <IconButton
-        className={styles.zoomBtn}
-        iconSrc="/icons/magnifier.svg"
-        iconAlt="Zoom button - Magnifier"
-        onClick={toggleModal}
-      />
-      <div className={styles.info}>
-        <span>Photographer:</span>
-        <p>{photoData.photographer}</p>
-      </div>
-    </li>
+    </>
   );
 }
